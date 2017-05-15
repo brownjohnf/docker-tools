c6da17b3ab492096cd5bf055b9d1d5fac90621d4
John (Jack) Brown <jack@brownjohnf.com>
2017-05-15 08:51:53 -0700

    Add findParent

---
+++ findParent.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const
+  Promise = require('bluebird'),
+  { execAsync } = Promise.promisifyAll(require('child_process'))
+
+const
+  startingImage = process.argv[2]
+
+console.log(startingImage)
+
+const getParent = (imageId) => {
+  return execAsync(`docker inspect ${imageId}`)
+  .then(json => {
+    const image = JSON.parse(json)[0]
+    console.log(image)
+
+    if (image.Parent === '') {
+      return image
+    }
+
+    return getParent(image.Parent)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+}
+
+getParent(startingImage)
+.then(image => {
+  console.log(image.RepoTags)
+})
+
